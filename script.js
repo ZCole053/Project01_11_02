dd3e463826814085f0b019a15e5e0900014bd181
Unknown <zcole053@west-mec.org>
2018-09-04 15:29:14 -0700

    Validation
    I added several functions that shut off default validations, quotes and other things.

--- script.js
+++ script.js
@@ -1,9 +1,72 @@
 /*  Project 01_11_02
 
-    Author: 
-    Date:   
+    Author: Zedekiah Cole 
+    Date:   9/4/18
 
     Filename: script.js
 */
 
 "use strict";
+
+/* Global variables */
+var httpRequest = false;
+var entry = "^IXIC";
+
+
+// this function request the option 
+function getRequestObject(){
+    try{
+        httpRequest = new XMLHttpRequest();
+    }
+    catch(requestError){
+        return false;
+    }
+    return httpRequest;
+}
+
+
+// this function stops all default validations
+function stopSubmission(evt){
+    if(evt.preventDefault){
+        evt.PreventDefault();
+    }
+    else{
+        evt.returnValue = false;
+    }
+    getQuote();
+}
+
+
+function getQuote(){
+    if(document.getElementsByTagName("input")[0].value){
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if(!httpRequest){
+        httpRequest = getRequestObject();
+    }
+
+    httpRequest.abort();
+    httpRequest.open("get", "StockCheck.php?t=" + entry, true);
+    httpRequest.send(null);
+}
+
+/* eventhandlers */
+
+// this eventhandler activates when it loads and calls the functions stopSubmission and getRequestObject
+var form= document.getElementsByTagName("form")[0];
+if(form.addEventListener){
+    form.addEventListener("submit",stopSubmission,false);
+    window.addEventListener("load",getQuote,false);
+}
+else if(form.attachEvent){
+    form.attachEvent("onsubmit", stopSubmission);
+    window.addEventListener("onload",getQuote);
+}
+
+
+if(window.addEventListener){
+    window.addEventListener("load",getRequestObject,false);
+}
+else if(window.attachEvent){
+    window.attachEvent("onload", getRequestObject);
+}
