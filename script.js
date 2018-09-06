9926d7d23bc090a3a3822ef46c67e78d900fe1fe
Unknown <zcole053@west-mec.org>
2018-09-06 15:24:55 -0700

    Api change
    We changed the entire function for it to accept the json data that we are getting because yahoo dropped it's api. Then we changed the php file to accept the new api that we found on the web.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
 /* Global variables */
 var httpRequest = false;
-var entry = "^IXIC";
+var entry = "MSFT";
 
 
 // this function request the option 
@@ -38,6 +38,8 @@ function stopSubmission(evt) {
 function getQuote() {
     if (document.getElementsByTagName("input")[0].value) {
         entry = document.getElementsByTagName("input")[0].value;
+    } else {
+        document.getElementsByTagName("input")[0].value = entry;
     }
     if (!httpRequest) {
         httpRequest = getRequestObject();
@@ -48,26 +50,24 @@ function getQuote() {
     httpRequest.send(null);
     httpRequest.onreadystatechange = displayData;
     clearTimeout(updateQuote);
-    var updateQuote = setTimeout('getQuote()', 100000);
+    var updateQuote = setTimeout('getQuote()', 10000);
 }
 
 // this function takes the data and puts it in the dom
 function displayData() {
     if (httpRequest.readyState == 4 && httpRequest.status === 200) {
-        var stockResults = httpRequest.reponseText;
-        var stockItems = stockResults.split(/,|\"/);
-        for (var i = stockItems.length - 1; i >= 0; i--) {
-            if (stockItems[i] === "") {
-                stockItems.splice(i, 1)
-            }
-        }
-        document.getElementById("ticker").innerHTML = stockItems[0];
-        document.getElementById("openingprice").innerHTML = stockItems[6];
-        document.getElementById("lastTrade").innerHTML = stockItems[1];
-        document.getElementById("lastTradeDT").innerHTML = stockItems[2] + "," + stockItems[3];
-        document.getElementById("change").innerHTML = stockItems[4];
-        document.getElementById("range").innerHTML = (stockItems[8] * 1).toFixed(2) + "&ndash;" + (stockItems[7] * 1).toFixed(2);
-        document.getElementById("volume").innerHTML = (stockItems[9] * 1).toLocaleString();
+        var stockResults = httpRequest.responseText;
+        var stockItems = JSON.parse(stockResults);
+        console.log(stockItems);
+
+        document.getElementById("ticker").innerHTML = stockItems.symbol;
+        document.getElementById("openingPrice").innerHTML = stockItems.open;
+        document.getElementById("lastTrade").innerHTML = stockItems.latestPrice;
+        var date = new Date(stockItems.latestUpdate);
+        document.getElementById("lastTradeDT").innerHTML = date.toDateString() + "<br>" + date.toLocaleTimeString();
+        document.getElementById("change").innerHTML = (stockItems.latestPrice - stockItems.open).toFixed(2);
+        document.getElementById("range").innerHTML = "Low " + (stockItems.low * 1).toFixed(2) + "<br>High " + (stockItems.high * 1).toFixed(2);
+        document.getElementById("volume").innerHTML = (stockItems.latestVolume * 1).toLocaleString();
     }
 }
 
